31c8fc878aa7fde97ae509bc95cdc06b85eeb190
islamosman2541998 <104398640+islamosman2541998@users.noreply.github.com>
2026-01-15 10:55:59 +0200

    Update Tokens.php

--- api/Tokens.php
+++ api/Tokens.php
@@ -1,13 +1,15 @@
 <?php
-
 class Tokens extends ApiController
 {
+
     public $messaging;
+
 
     public function __construct()
     {
         $this->model = $this->model('Token');
     }
+
 
     public function send()
     {
@@ -15,74 +17,99 @@ class Tokens extends ApiController
 
         $title = $fields['title'];
         $body = $fields['body'];
-        $donor_id = isset($_POST['donor_id']) ? $_POST['donor_id'] : null;
-
-        if (!$donor_id) {
-            $this->error('donor_id is required');
-            return;
-        }
 
         $data = [
             'title'    => $title,
             'body'     => $body,
-            'donor_id' => $donor_id
+            'donor_id' => $_POST['donor_id']
         ];
 
         $jsonData = json_encode($data);
 
+
         $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_URL            => NOTIFICATION_DOMAIN . '/api/sendPush',
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => NOTIFICATION_DOMAIN . '/api/sendPush',
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING       => '',
-            CURLOPT_MAXREDIRS      => 10,
-            CURLOPT_TIMEOUT        => 30,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST  => 'POST',
-            CURLOPT_POSTFIELDS     => $jsonData,
-            CURLOPT_HTTPHEADER     => [
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $jsonData,
+            CURLOPT_HTTPHEADER => array(
                 'Content-Type: application/json',
-                'Accept: application/json',
-            ],
-        ]);
+                'Cookie: PHPSESSID=d27f437f16b9e581731a8a46da6e1832'
+            ),
+        ));
 
         $response = curl_exec($curl);
-        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         $err = curl_error($curl);
         curl_close($curl);
 
-        if ($err) {
-            $this->error('Failed to send notification: ' . $err);
-            return;
-        }
-
-        $responseData = json_decode($response, true);
-
-        $this->response([
-            'success'      => $httpCode === 200,
-            'http_code'    => $httpCode,
-            'sent_data'    => $data,
-            'fcm_response' => $responseData
-        ]);
+        $this->response($data);
     }
+
 
     public function save()
     {
+        //validate all data
         $fields = $this->requiredArray(['device_id', 'fcm_token']);
 
+        if ($this->model->getToken($_POST['device_id'])) {
+
+            $data = [
+                'device_id' => $_POST['device_id'],
+                'fcm_token' => $_POST['fcm_token'],
+                'donor_id' => $_POST['donor_id'],
+            ];
+
+            //save donor
+            $data['token_id'] = $this->model->updateToken($data);
+            $this->response($data);
+        }
+        //prepar data for save
         $data = [
             'device_id' => $_POST['device_id'],
             'fcm_token' => $_POST['fcm_token'],
-            'donor_id'  => $_POST['donor_id'] ?? null,
+            'donor_id' => $_POST['donor_id'],
         ];
 
-        if ($this->model->getToken($_POST['device_id'])) {
-            $data['token_id'] = $this->model->updateToken($data);
-        } else {
-            $data['token_id'] = $this->model->addToken($data);
-        }
-
+        //save donor
+        $data['token_id'] = $this->model->addToken($data);
+        
         $this->response($data);
     }
+
+    // public function save()
+    // {
+    //     //validate all data
+    //     $fields = $this->requiredArray(['device_id', 'fcm_token']);
+
+    //     if ($this->model->getToken($_POST['device_id'])) {
+
+    //         $data = [
+    //             'device_id' => $_POST['device_id'],
+    //             'fcm_token' => $_POST['fcm_token'],
+    //             'donor_id' => $_POST['donor_id'],
+    //         ];
+
+    //         //save donor
+    //         $data['token_id'] = $this->model->updateToken($data);
+    //         $this->response($data);
+    //     }
+    //     //prepar data for save
+    //     $data = [
+    //         'device_id' => $_POST['device_id'],
+    //         'fcm_token' => $_POST['fcm_token'],
+    //         'donor_id' => $_POST['donor_id'],
+    //     ];
+
+    //     //save donor
+    //     $data['token_id'] = $this->model->addToken($data);
+
+
+    //     $this->response($data);
+    // }
 }
